36ca80e9995cf3f438e4b2d5b3b60c661b726055
sharon <sharonsujee36@gmail.com>
2019-09-23 16:33:50 +0530

    Initial commit

---
+++ bot.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+"""Example bot that returns a synchronous response."""
+
+from flask import Flask, request, json
+from api import server_api
+import requests
+
+
+app = Flask(__name__)
+
+apis = server_api()
+@app.route('/', methods=['POST'])
+def on_event():
+  """Handles an event from Hangouts Chat."""
+  event = request.get_json()
+  if event['type'] == 'ADDED_TO_SPACE' and event['space']['type'] == 'ROOM':
+    message = 'Thanks for adding me to "%s"!' % event['space']['displayName']
+  elif event['type'] == 'MESSAGE':
+    #bot logic
+    message = ""
+    action = ""
+    command = event['message']['text']
+    user_action = command.split('-').lower()
+    if user_action == 'start':
+      action = 'Started'
+    elif user_action == 'stop':
+      action = 'Stoppped'
+    
+    if check_command(user_action):
+      call_api(user_action)
+    else:
+        message = message + " I do not understand your command, please refer the guide and retry! or you can ask sharon for help ! :)"
+        return { "text": message }
+
+  else:
+    return
+  return json.jsonify({'text': message})
+
+
+def check_command(user_action):
+  return user_action in apis
+
+def call_api(user_action):
+  command = apis[user_action][0]
+  server_name = apis[user_action][2]
+  url = command
+  response = requests.get(url)
+  response_data = response.json()
+  code  = response_data['code']
+  if code == 200:
+    status = "success!"
+  else:
+    status = "something went wrong!,please contact sharon!"
+  
+  message = response.getContentText();
+  message = "Command :" + request + "\nCommand executed " + "by: " + event.user.displayName +"\n" + "Server :" + server_name + "\nServer Status : " + action + "\nResponse Code: " + response.getResponseCode() + "\nResult: " + status;
+
+
+if __name__ == '__main__':
+  app.run(port=8080, debug=True)
